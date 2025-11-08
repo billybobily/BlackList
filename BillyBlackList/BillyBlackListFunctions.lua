3e6fbaa5b618ba197fb4e8c06866dacd9b403a2c
BillyBobily <billybobily.twow@gmail.com>
2025-11-08 16:56:28 -0500

    fix base64

--- BillyBlackList/BillyBlackListFunctions.lua
+++ BillyBlackList/BillyBlackListFunctions.lua
@@ -457,15 +457,15 @@ local b64chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
 -- Base64 encode function
 local function base64_encode(data)
 	return ((data:gsub('.', function(x) 
-		local r,b='',x:byte()
+		local r,b='',string.byte(x)
 		for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
 		return r;
 	end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-		if (#x < 6) then return '' end
+		if (string.len(x) < 6) then return '' end
 		local c=0
-		for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-		return b64chars:sub(c+1,c+1)
-	end)..({ '', '==', '=' })[#data%3+1])
+		for i=1,6 do c=c+(string.sub(x,i,i)=='1' and 2^(6-i) or 0) end
+		return string.sub(b64chars,c+1,c+1)
+	end)..({ '', '==', '=' })[string.len(data)%3+1])
 end
 
 -- Base64 decode function
@@ -473,13 +473,13 @@ local function base64_decode(data)
 	data = string.gsub(data, '[^'..b64chars..'=]', '')
 	return (data:gsub('.', function(x)
 		if (x == '=') then return '' end
-		local r,f='',(b64chars:find(x)-1)
+		local r,f='',(string.find(b64chars,x)-1)
 		for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
 		return r;
 	end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-		if (#x ~= 8) then return '' end
+		if (string.len(x) ~= 8) then return '' end
 		local c=0
-		for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+		for i=1,8 do c=c+(string.sub(x,i,i)=='1' and 2^(8-i) or 0) end
 		return string.char(c)
 	end))
 end
